f70177ba58b3c398017a9c72a7cc0d3a056e4531
Pepich <benedikt.abel@yahoo.de>
2016-11-18 14:09:54 +0100

    Added missing documentation

--- src/com/redstoner/remote_console/utils/ConsoleReader.java
+++ src/com/redstoner/remote_console/utils/ConsoleReader.java
@@ -9,14 +9,26 @@ import org.apache.logging.log4j.core.layout.PatternLayout;
 import com.redstoner.remote_console.protected_classes.Main;
 import com.redstoner.remote_console.protected_classes.UserManager;
 
+/**
+ * This class is responsible for grabbing, processing and forwarding the console output to the UserManager.
+ * 
+ * @author Pepich1851
+ */
+
 public class ConsoleReader extends AbstractAppender
 {
+	/**
+	 * Constructor, creates the log appender to read the console output.
+	 */
 	protected ConsoleReader()
 	{
 		super("Log4JAppender", null, PatternLayout.createLayout("[%d{HH:mm:ss} %level]: %msg", null, null, null, null),
 				false);
 	}
 	
+	/**
+	 * Initialize the console reader, appends it to the logger.
+	 */
 	public static void init()
 	{
 		Main.logger.info("Initializing console reader.");
@@ -25,12 +37,18 @@ public class ConsoleReader extends AbstractAppender
 		log.addAppender(reader);
 	}
 	
+	/**
+	 * Always running, return true.
+	 */
 	@Override
 	public boolean isStarted()
 	{
 		return true;
 	}
 	
+	/**
+	 * Gets called when a logevent happens. Notifies the usermanager about the event.
+	 */
 	@Override
 	public void append(LogEvent event)
 	{
